c1894445c6bb60bc10bbfb4d83aca07d21487b90
Hicham-Yezza@github.io <Hicham.Yezza@gmail.com>
2023-09-05 21:46:18 +0100

    Updating eda.py, merging with load_datasets.py

--- eda.py
+++ eda.py
@@ -1,66 +1,93 @@
 # eda.py - Utility script for exploratory data analysis
 
 # Import necessary libraries
-import pandas as pd
 import matplotlib.pyplot as plt
+from datasets import load_dataset
 
-# Import the load_cnn_dailymail and load_xsum functions from load_datasets.py
-from load_datasets import load_cnn_dailymail, load_xsum
+# Configuration for the histograms
+# This makes it easy to adjust settings for each dataset
+HISTOGRAM_CONFIG = {
+    'cnn_dailymail': {'bins': 40, 'range': (0, 1300)},
+    'xsum': {'bins': 20, 'range': (0, 80)}
+}
 
-# Loading the CNN/Daily Mail and XSum datasets
-cnn_dailymail = load_cnn_dailymail()
-xsum = load_xsum()
+def load_and_print_dataset_info(dataset_name):
+    """
+    Load a dataset and print its basic information.
+    
+    Parameters:
+        dataset_name (str): Name of the dataset to be loaded.
 
-# Printing basic information about the loaded datasets, such as their names and versions
-print("CNN/Daily Mail dataset:")
-print(cnn_dailymail)
-print("\nXSum dataset:")
-print(xsum)
+    Returns:
+        DatasetDict: The dataset loaded using HuggingFace's datasets library.
+    """
+    try:
+        dataset = load_dataset(dataset_name)
+        print(f"\n{dataset_name} dataset:")
+        # Loop through each split (train, test, validation) and print its size
+        for split, data in dataset.items():
+            print(f"{split} size: {len(data)}")
+        return dataset
+    except Exception as e:
+        print(f"Error loading {dataset_name} dataset: {e}")
+        return None
 
-# Accessing the "train" split of both the CNN/Daily Mail and XSum datasets
-train_cnn_dailymail = cnn_dailymail["train"]
-train_xsum = xsum["train"]
+def print_example_texts_and_summaries(dataset, article_key, summary_key):
+    """
+    Print example texts and summaries from a dataset.
+    
+    Parameters:
+        dataset (DatasetDict): The dataset from which to extract example texts and summaries.
+        article_key (str): The key to access the articles in the dataset.
+        summary_key (str): The key to access the summaries in the dataset.
+    """
+    example_text = dataset[article_key][0]
+    example_summary = dataset[summary_key][0]
 
-# Print the number of examples in the "train" split of each dataset
-print("\nNumber of examples in CNN/Daily Mail train split:", len(train_cnn_dailymail))
-print("Number of examples in XSum train split:", len(train_xsum))
+    print("\nExample text:")
+    print(example_text)
+    print("\nExample summary:")
+    print(example_summary)
 
-# Accessing example texts and summaries from the first example in each dataset
-example_text_cnn_dailymail = train_cnn_dailymail["article"][0]
-example_summary_cnn_dailymail = train_cnn_dailymail["highlights"][0]
-example_text_xsum = train_xsum["document"][0]
-example_summary_xsum = train_xsum["summary"][0]
+def calculate_summary_lengths(dataset, summary_key):
+    """
+    Calculate lengths of summaries based on word count.
+    """
+    return [len(summary.split()) for summary in dataset[summary_key]]
 
-# Printing example texts and summaries for both datasets
-print("\nExample text (CNN/Daily Mail):")
-print(example_text_cnn_dailymail)
-print("\nExample summary (CNN/Daily Mail):")
-print(example_summary_cnn_dailymail)
-print("\nExample text (XSum):")
-print(example_text_xsum)
-print("\nExample summary (XSum):")
-print(example_summary_xsum)
+def plot_summary_lengths_histogram(data, title, dataset_name, subplot_position):
+    """
+    Plot histogram for summary lengths based on provided configuration.
+    """
+    config = HISTOGRAM_CONFIG.get(dataset_name)
+    plt.subplot(*subplot_position)
+    plt.hist(data, bins=config['bins'], range=config['range'], edgecolor="k", alpha=0.7)
+    plt.xlabel("Summary Length")
+    plt.ylabel("Frequency")
+    plt.title(title)
 
-# Performing exploratory data analysis (EDA)
-# Loading data from the datasets and analyzing it
-# Calculating and visualize summary lengths
-summary_lengths_cnn_dailymail = [len(summary.split()) for summary in train_cnn_dailymail["highlights"]]
-summary_lengths_xsum = [len(summary.split()) for summary in train_xsum["summary"]]
+if __name__ == "__main__":
+    # Load datasets using the helper function
+    cnn_dailymail = load_and_print_dataset_info("cnn_dailymail")
+    xsum = load_and_print_dataset_info("xsum")
 
-# Plot analysis results
-plt.figure(figsize=(10, 6))
+    # Accessing the "train" split for further analysis
+    train_cnn_dailymail = cnn_dailymail["train"]
+    train_xsum = xsum["train"]
 
-plt.subplot(2, 1, 1)
-plt.hist(summary_lengths_cnn_dailymail, bins=20, range=(0, 200), edgecolor="k", alpha=0.7)
-plt.xlabel("Summary Length")
-plt.ylabel("Frequency")
-plt.title("Distribution of Summary Lengths (CNN/Daily Mail)")
+    # Print example articles and summaries for both datasets
+    print("\nCNN/Daily Mail examples:")
+    print_example_texts_and_summaries(train_cnn_dailymail, "article", "highlights")
+    print("\nXSum examples:")
+    print_example_texts_and_summaries(train_xsum, "document", "summary")
 
-plt.subplot(2, 1, 2)
-plt.hist(summary_lengths_xsum, bins=20, range=(0, 50), edgecolor="k", alpha=0.7)
-plt.xlabel("Summary Length")
-plt.ylabel("Frequency")
-plt.title("Distribution of Summary Lengths (XSum)")
+    # Calculate summary lengths using the helper function
+    summary_lengths_cnn_dailymail = calculate_summary_lengths(train_cnn_dailymail, "highlights")
+    summary_lengths_xsum = calculate_summary_lengths(train_xsum, "summary")
 
-plt.tight_layout()
-plt.show()
+    # Plot histograms for summary lengths of each dataset
+    plt.figure(figsize=(10, 6))
+    plot_summary_lengths_histogram(summary_lengths_cnn_dailymail, "Distribution of Summary Lengths (CNN/Daily Mail)", 'cnn_dailymail', (2, 1, 1))
+    plot_summary_lengths_histogram(summary_lengths_xsum, "Distribution of Summary Lengths (XSum)", 'xsum', (2, 1, 2))
+    plt.tight_layout()
+    plt.show()
