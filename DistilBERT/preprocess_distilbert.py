fa5592b1c529d812481f7e5ff1ec1a4f6742e8df
Hicham-Yezza@github.io <Hicham.Yezza@gmail.com>
2023-09-07 18:20:05 +0100

    Refactor preprocess_distilbert.py - dropping CNN dataset for testing, adding comments

--- DistilBERT/preprocess_distilbert.py
+++ DistilBERT/preprocess_distilbert.py
@@ -1,21 +1,46 @@
-"""
-Preprocessing script for DistilBERT model.
-
-This script handles preprocessing of datasets to be compatible with DistilBERT model's requirements.
-"""
-
 from transformers import DistilBertTokenizer
+from datasets import load_dataset, DatasetDict
+import os
 
 tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 
-def preprocess_for_distilbert(dataset):
+DATASET_CONFIG = {
+    'cnn_dailymail': {'version': '3.0.0', 'article_key': 'article', 'summary_key': 'highlights'},
+    'xsum': {'version': None, 'article_key': 'document', 'summary_key': 'summary'}
+}
+
+def preprocess_for_distilbert(dataset, article_key, summary_key):
     """Tokenize data for DistilBERT."""
     def tokenize_example(example):
-        return tokenizer(example['article'], truncation=True, padding='max_length', max_length=512), tokenizer(example['summary'], truncation=True, padding='max_length', max_length=150)
-
+        return {
+            'input_article': tokenizer(example[article_key], truncation=True, padding='max_length', max_length=512),
+            'input_summary': tokenizer(example[summary_key], truncation=True, padding='max_length', max_length=150)
+        }
     return dataset.map(tokenize_example)
 
-# Preprocess datasets
-cnn_dailymail = preprocess_for_distilbert(cnn_dailymail)
-xsum = preprocess_for_distilbert(xsum)
- 
+def main():
+    datasets = {}
+    processed_datasets = {}
+
+    # Ensure there's a directory to save the processed data
+    if not os.path.exists('./processed_data'):
+        os.makedirs('./processed_data')
+
+    for dataset_name, config in DATASET_CONFIG.items():
+        try:
+            if config['version']:
+                dataset = load_dataset(dataset_name, config['version'])
+            else:
+                dataset = load_dataset(dataset_name)
+            
+            if dataset:
+                datasets[dataset_name] = dataset['train']
+                processed_dataset = preprocess_for_distilbert(dataset['train'], config['article_key'], config['summary_key'])
+                processed_datasets[dataset_name] = processed_dataset
+                processed_dataset.save_to_disk(f"./processed_data/{dataset_name}")
+
+        except Exception as e:
+            print(f"Error processing {dataset_name}: {str(e)}")
+
+if __name__ == "__main__":
+    main()
